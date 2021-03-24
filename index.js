5bbd9b4d61c609adb96bb73a665e8b6aca0aa23b
Alexandra Scott <alexandra.k.scott@gmail.com>
2021-03-24 10:25:58 -0500

    playing with identifiers

---
+++ index.js
@@ -0,0 +1,11 @@
+const pi =3.14;  
+console.log("The value of Pi is: "+pi);  
+  
+
+var name = "William";  
+console.log("Welcome to JS course, Mr." + name);  
+// eslint-disable-next-line no-redeclare
+var name = "Goth"; /* Here, even though we have redeclared the same identifier, it will not throw any error.*/ 
+console.log("Welcome to JS course, Mr." + name); 
+
+
