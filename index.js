4cedd86f17bc1f0ab02b6fdb25ca8d546775f3df
Alexandra Scott <alexandra.k.scott@gmail.com>
2021-03-24 10:32:05 -0500

    testing identifiers

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const pi =3.14;  
-console.log("The value of Pi is: "+pi);  
+// const pi =3.14;  
+// console.log("The value of Pi is: "+pi);  
   
 
-var name = "William";  
-console.log("Welcome to JS course, Mr." + name);  
-// eslint-disable-next-line no-redeclare
-var name = "Goth"; /* Here, even though we have redeclared the same identifier, it will not throw any error.*/ 
-console.log("Welcome to JS course, Mr." + name); 
+// var name = "William";  
+// console.log("Welcome to JS course, Mr." + name);  
+// // eslint-disable-next-line no-redeclare
+// var name = "Goth"; /* Here, even though we have redeclared the same identifier, it will not throw any error.*/ 
+// console.log("Welcome to JS course, Mr." + name); 
+var tempFahrenheit=99; 
+                   
+let TempFahrenheit=99; 
+//let TempFahrenheit=99; Redeclaration is not allowed 
+
+const TEMP_CELSIUS = 38;  
+					
+// document.write() is used to write content onto the HTML page
+document.write("Default temperature (var) is: <span class=\"blinking\">"+tempFahrenheit+"</span>"); 
+document.write("<br/>"); 
+document.write("Default temperature (let) is: <span class=\"blinking\">"+TempFahrenheit+"</span>"); 
+document.write("<br/>"); 
+document.write("Normal body temperature in Celsius is: <span class=\"blinking\">" + TEMP_CELSIUS +  "</span>"); 
+               
 
 
