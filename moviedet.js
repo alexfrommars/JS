d703ae28125b2b2789c36fa2e76f5152fab9f521
Alexandra Scott <alexandra.k.scott@gmail.com>
2021-03-31 12:23:23 -0500

    type practice and template literals

---
+++ moviedet.js
@@ -0,0 +1,10 @@
+//display movie details. init variables with values of appropriate types
+let movie = "Twilight";
+let language = "English";
+let ratings = 4.5;
+document.write("Movie : "+movie+"");
+document.write("Language : "+language+"");
+document.write("Ratings : "+ratings+"");
+
+//maybe could try:
+//document.getElementById('maincontent').innerHTML = "Movie Name: " + movieName.Language+""+"Rating: " +movieName.Rating + "" +"Year: " + movieName.Year;
